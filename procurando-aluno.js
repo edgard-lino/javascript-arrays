4ad974499b83ab722456d86dcb4f4611149a2af6
Edgard Lino da Silva <mosmack@gmail.com>
2022-12-10 22:07:55 -0300

    procurando-aluno

---
+++ procurando-aluno.js
@@ -0,0 +1,17 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+function exibeNomeENota (aluno) {
+  if (listaDeAlunosEMedias[0].includes(aluno)) {
+    const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+    const mediaDoAluno = listaDeAlunosEMedias[1][indice];
+
+    console.log(`${aluno} está cadastrado e sua média é ${mediaDoAluno}.`);
+  } else {
+    console.log(`Aluno não cadastrado.`);
+  }
+};
+
+exibeNomeENota("João");
