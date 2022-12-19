a2543e23adb7fb96522accca952d6097880d4aaa
Edgard Lino da Silva <mosmack@gmail.com>
2022-12-18 21:39:51 -0300

    strings são arrays?

---
+++ string-sao-arrays.js
@@ -0,0 +1,20 @@
+// const nome = "Alura";
+// ["A", "l", "u", "r", "a"]
+
+
+
+const nome = "Alura";
+let nomeMaiusculas = "";
+
+for (let i = 0; i < nome.length; i++) {
+ nomeMaiusculas += nome[i].toUpperCase()
+}
+console.log(nomeMaiusculas) //ALURA
+
+
+
+const nomedoCurso = "Fundamentos de JS";
+const nomeDaPlataforma = " Alura"
+
+const nomeCompleto = nomedoCurso.concat(nomeDaPlataforma)
+console.log(nomeCompleto)  //Fundamentos de JS Alura
